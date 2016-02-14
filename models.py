83aea17c5e75c161c3ae5867e984de6f2afd7024
xyder <xydr2311@gmail.com>
2016-02-14 21:51:33 +0200

    Removed position recount functionality and placed it in server project.

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, Text, ForeignKey, DateTime, Enum
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.event import listen
 
-from core.database import Base, db_session
+from core.database import Base, db_session, recount_positions
 from core.lib import get_custom_prefixer
 from xds_bookmarks import APP_NAME
 
@@ -46,10 +46,14 @@ class Bookmark(Base):
     date_added = Column(DateTime)
     date_modified = Column(DateTime)
 
-    type = Column(Enum("container", "item", name='bookmark_types'), nullable=False)
+    type = Column(Enum('container', 'item', name='bookmark_types'), nullable=False)
 
     location_id = Column(Integer, ForeignKey(prefixer('locations.id')), index=True)
     location = relationship('Location', backref='bookmarks')
+
+    __mapper_args__ = {
+        'confirm_deleted_rows': False
+    }
 
     def __repr__(self):
         return self.title
@@ -110,66 +114,5 @@ class Pane(Base):
         return '{"id": "%s", "bookmark_id": "%s"}' % (self.id, self.bookmark_id)
 
 
-def recount_positions(bookmark, is_deleted):
-    """
-    Function that recounts the relative positions of the children of the parent of the given bookmark.
-    :param is_deleted: if the bookmark is marked for deletion.
-    """
-
-    children = Bookmark.query.filter(
-        Bookmark.parent == bookmark.parent
-    ).order_by(Bookmark.position.asc()).all()
-
-    if bookmark in children:
-        children.remove(bookmark)
-
-    k = 0
-    n = len(children)
-
-    if not is_deleted:
-        if bookmark.position is None or bookmark.position > n:
-            bookmark.position = n
-
-        if bookmark.position < 0:
-            bookmark.position = 0
-
-    for child in children:
-        # skip bookmark if not deleted
-        if k == bookmark.position and not is_deleted:
-            k += 1
-
-        child.position = k
-        k += 1
-
-
-def process_session_objects(session, objects, parents, is_deleted):
-    """
-    Function that processes a set of session objects.
-    :param parents: the bookmark parents for which the children were already recounted.
-    :param is_deleted: if the objects in the set were marked for deletion
-    :return: expanded list of parents for which the children were recounted.
-    """
-
-    for obj in objects:
-        if isinstance(obj, Bookmark) and obj.parent not in parents:
-            if session.is_modified(obj) or is_deleted:
-                recount_positions(obj, is_deleted)
-                parents.append(obj.parent)
-    return parents
-
-
-# listeners
-# noinspection PyUnusedLocal
-def before_session_flush(session, flush_context, instances):
-    """
-    Function that processes session objects before they are stored in the database.
-    """
-
-    parents = []
-    parents = process_session_objects(session, session.new, parents, False)
-    parents = process_session_objects(session, session.dirty, parents, False)
-    process_session_objects(session, session.deleted, parents, True)
-
-
-# listener registration
-listen(db_session, 'before_flush', before_session_flush)
+# register recounting listeners for these models
+Base.register_recountable_model(Bookmark)
